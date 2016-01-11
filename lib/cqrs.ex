29bfb1b9548dc1fc34879ee4c2d31cc9e944e666
Mohamed Boudra <boudra.moha@gmail.com>
2016-01-11 13:54:03 +0100

    commands, events & a test

--- lib/cqrs.ex
+++ lib/cqrs.ex
@@ -1,37 +1,80 @@
+defmodule Cqrs do
+
+  defmodule Event, do: defstruct uuid: nil, timestamp: nil, payload: nil
+
+  def uuid, do: Kernel.make_ref
+
+  def event(payload) do
+    %Event{
+      uuid: uuid,
+      timestamp: :os.system_time(:seconds),
+      payload: payload
+    }
+  end
+
+end
+
 defmodule User do
 
   defstruct name: nil, uuid: nil, changes: []
 
-  defmodule Events do
-    defmodule Created do
-      defstruct name: nil, uuid: nil
-    end
-  end
-
-  defmodule Commands do
-    defmodule Create do
-      defstruct name: nil, uuid: nil
-    end
-  end
-
-  def dispatch(user, message) do
-    user = %{user | changes: [message|user.changes]}
+  def apply(user, message) do
+    user = %{user | changes: [ Cqrs.event(message) | user.changes]}
+    IO.inspect message
     handle(user, message)
   end
 
-  def handle(user, %Events.Created{name: name, uuid: uuid}) do
-    %{user | name: name, uuid: uuid}
+  def handle(user, {:created, e}) do
+    %{user | name: e.name, uuid: e.uuid}
+  end
+
+  def handle(user, {:name_changed, e}) do
+    %{user | name: e.new_name}
   end
 
   def new do
     %User{}
   end
 
+  def create(user, name) do
+    User.apply(user, { :created, %{name: name, uuid: Cqrs.uuid } })
+  end
+
+  def change_name(user, new_name) do
+    User.apply(user, { :name_changed, %{new_name: new_name, uuid: user.uuid } })
+  end
+
 end
 
-defmodule UserRepository do
+defmodule UserCommandHandler do
 
   use GenServer
+
+  def handle_cast({ type, message }, state) do
+    user = (Map.has_key?(message, :uuid)
+           && Map.get(state, message[:uuid]) || User.new)
+           |> UserCommandHandler.command({ type, message })
+    IO.inspect user.name
+    {:noreply, Map.put(state, user.uuid, user)}
+  end
+
+  def handle_call({ :get_state }, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call({ :find_by_id, uuid }, _from, state) do
+    {:reply, Map.get(state, uuid), state}
+  end
+
+  def command(user, { :create, %{ name: name } }) do
+    User.create(user, name)
+  end
+
+  def command(user, { :change_name, c }) do
+    User.change_name(user, c.new_name)
+  end
+
+  ## Client API
 
   def start_link do
     GenServer.start_link(__MODULE__, :ok, [])
@@ -41,26 +84,12 @@ defmodule UserRepository do
     {:ok, %{}}
   end
 
-  def handle_cast({:command, message}, state) do
-    IO.inspect "Got #{inspect message} in process #{inspect self()}"
-    user = command(message)
-    {:noreply, Map.put(state, user.uuid, user)}
+  def all(repository) do
+    GenServer.call(repository, { :get_state }) |> Map.values
   end
 
-  def handle_call({:hola}, _wat, state) do
-    {:reply, state, state}
-  end
-
-  def command(%User.Commands.Create{name: name, uuid: uuid}) do
-    User.new |> User.dispatch(%User.Events.Created{name: name, uuid: uuid})
-  end
-
-  def create(repository, name) do
-    GenServer.cast(repository, {:command, %User.Commands.Create{name: name, uuid: Kernel.make_ref}})
-  end
-
-  def get_all(repository) do
-    GenServer.call(repository, {:hola})
+  def find(repository, uuid) do
+    GenServer.call(repository, { :find_by_id, uuid })
   end
 
 end
@@ -69,17 +98,12 @@ defmodule Main do
   use Application
 
   def start(_type, _args) do
-    # import Supervisor.Spec, warn: false
-    # children = [
-    #   worker(UserRepository, [])
-    # ]
-    # opts = [strategy: :one_for_one, name: Main.Supervisor]
-    # Supervisor.start_link(children, opts)
-    {:ok, pid} = UserRepository.start_link
-    UserRepository.create(pid, "hola")
-    all = UserRepository.get_all(pid)
-    IO.inspect all
-    {:ok, pid}
+    import Supervisor.Spec, warn: false
+    children = [
+      worker(UserCommandHandler, [])
+    ]
+    opts = [strategy: :one_for_one, name: Main.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 
 end
--- test/cqrs_test.exs
+++ test/cqrs_test.exs
@@ -2,8 +2,13 @@ defmodule CqrsTest do
   use ExUnit.Case
   doctest User
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "commands" do
+    {:ok, pid} = UserCommandHandler.start_link
+    GenServer.cast(pid, { :create, %{ name: "John" } })
+    uuid = UserCommandHandler.all(pid) |> List.first |> Map.get(:uuid)
+    GenServer.cast(pid, { :change_name, %{ uuid: uuid, new_name: "Jack" } })
+    assert UserCommandHandler.find(pid, uuid).name == "Jack"
+    {:ok, pid}
   end
 
 end
