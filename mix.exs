8314d4a538a9b642da8716cf093880e01aac2d01
Mohamed Boudra <boudra.moha@gmail.com>
2016-01-11 14:43:11 +0100

    excoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Cqrs.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test],
+   ]
   end
 
   # Configuration for the OTP application
@@ -15,7 +18,7 @@ defmodule Cqrs.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
-     mod: {Main, []}]
+      mod: {Main, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +31,6 @@ defmodule Cqrs.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:excoveralls, "~> 0.4", only: :test}]
   end
 end
