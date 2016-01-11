bf83e9fa0d16f36153b404182184578a0580e6cd
Mohamed Boudra <boudra.moha@gmail.com>
2016-01-11 17:24:20 +0100

    macros for declaring event & command handlers

--- lib/cqrs.ex
+++ lib/cqrs.ex
@@ -2,9 +2,35 @@ defmodule Cqrs do
 
   defmodule Event, do: defstruct uuid: nil, timestamp: nil, payload: nil
 
+  defmacro __using__(_) do
+    quote do
+      import Cqrs
+
+      def apply(user, message) do
+        %{handle(user, message) | changes: [ make_event(message) | user.changes]}
+      end
+    end
+  end
+
+  defmacro event(type, expr) do
+    quote do
+      def handle(var!(user), { unquote(:"#{type}"), var!(e) }) do
+        unquote(expr)[:do]
+      end
+    end
+  end
+
+  defmacro command(type, expr) do
+    quote do
+      def handle(var!(user), { unquote(:"#{type}"), var!(e) }) do
+        unquote(expr)[:do]
+      end
+    end
+  end
+
   def uuid, do: Kernel.make_ref
 
-  def event(payload) do
+  def make_event(payload) do
     %Event{
       uuid: uuid,
       timestamp: :os.system_time(:seconds),
@@ -16,25 +42,14 @@ end
 
 defmodule User do
 
+  use Cqrs
+
   defstruct name: nil, uuid: nil, changes: []
 
-  def apply(user, message) do
-    user = %{user | changes: [ Cqrs.event(message) | user.changes]}
-    IO.inspect message
-    handle(user, message)
-  end
+  event :created, do: %{user | name: e.name, uuid: e.uuid}
+  event :name_changed, do: %{user | name: e.new_name}
 
-  def handle(user, {:created, e}) do
-    %{user | name: e.name, uuid: e.uuid}
-  end
-
-  def handle(user, {:name_changed, e}) do
-    %{user | name: e.new_name}
-  end
-
-  def new do
-    %User{}
-  end
+  def new, do: %User{}
 
   def create(user, name) do
     User.apply(user, { :created, %{name: name, uuid: Cqrs.uuid } })
@@ -49,12 +64,13 @@ end
 defmodule UserCommandHandler do
 
   use GenServer
+  import Cqrs
 
   def handle_cast({ type, message }, state) do
     user = (Map.has_key?(message, :uuid)
            && Map.get(state, message[:uuid]) || User.new)
-           |> UserCommandHandler.command({ type, message })
-    IO.inspect user.name
+           |> UserCommandHandler.handle({ type, message })
+    # IO.inspect user.name
     {:noreply, Map.put(state, user.uuid, user)}
   end
 
@@ -66,30 +82,25 @@ defmodule UserCommandHandler do
     {:reply, Map.get(state, uuid), state}
   end
 
-  def command(user, { :create, %{ name: name } }) do
-    User.create(user, name)
-  end
-
-  def command(user, { :change_name, c }) do
-    User.change_name(user, c.new_name)
-  end
-
   ## Client API
 
-  def start_link do
-    GenServer.start_link(__MODULE__, :ok, [])
-  end
+  def start_link, do: GenServer.start_link(__MODULE__, :ok, [])
+  def init(:ok), do: {:ok, %{}}
 
-  def init(:ok) do
-    {:ok, %{}}
-  end
-
-  def all(repository) do
+  def all(repository), do:
     GenServer.call(repository, { :get_state }) |> Map.values
+
+  def find(repository, uuid), do:
+    GenServer.call(repository, { :find_by_id, uuid })
+
+  ## Commands
+
+  command :create do
+    User.create(user, e.name)
   end
 
-  def find(repository, uuid) do
-    GenServer.call(repository, { :find_by_id, uuid })
+  command :change_name do
+    User.change_name(user, e.new_name)
   end
 
 end
