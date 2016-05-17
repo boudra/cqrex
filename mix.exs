b45fbbed60b8e59235e316a31e51a6c68571072e
Mohamed Boudra <boudra.moha@gmail.com>
2016-05-17 22:03:13 +0200

    populate required hex project fields

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Cqrex.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
+     description: description,
+     package: package,
      test_coverage: [tool: ExCoveralls],
      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test],
    ]
